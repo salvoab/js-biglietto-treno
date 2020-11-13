a325a23c0b47b3d9a458a415694ee04a125d0df9
salvoab <salvatore.abate85@gmail.com>
2020-11-13 16:27:19 +0100

    Esercizio completato
    L'output all'utente viene dato non più su console.log ma sulla pagina html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,38 @@
  * va applicato uno sconto del 40% per gli over 65
  *******************************************/
 
- var kmViaggio, etaPasseggero;
+var kmViaggio, etaPasseggero;
 kmViaggio = prompt("Inserisci il numero di km da percorrere");
 etaPasseggero = prompt("Inserisci la tua età");
  //if di validazione dei dati (mi devo accertare che l'utente inserisca numeri per kmViaggio ed etaPasseggero)
 if( !isNaN(kmViaggio) && !isNaN(etaPasseggero) ){
     kmViaggio=Number(kmViaggio);
     etaPasseggero=Number(etaPasseggero);
-    console.log(kmViaggio, etaPasseggero);
+    //console.log(kmViaggio, etaPasseggero);
+    document.getElementById("km").innerHTML = kmViaggio;
+    document.getElementById("eta").innerHTML = etaPasseggero;
+
     var prezzoAlKm = 0.21;
     var prezzoBiglietto = kmViaggio * prezzoAlKm;
-    console.log("Prezzo del biglietto: " + prezzoBiglietto + "€");
+    //console.log("Prezzo del biglietto: " + prezzoBiglietto + "€");
+    document.getElementById("prezzo_pieno").innerHTML = prezzoBiglietto.toFixed(2);
+
     //Verifico se è necessario applicare uno sconto
     var sconto = 0;
     if(etaPasseggero < 18 ){
         //Passeggero minorenne e applico lo sconto del 20%
         sconto = prezzoBiglietto * 20 / 100;
-        console.log("Sconto del 20%: " + sconto);
+        //console.log("Sconto del 20%: " + sconto);
     } else if(etaPasseggero >= 65){
-        //Passeggero over 65 (lo interpreto come età a partire dai 65 anni in su)
+        //Passeggero over 65 (lo interpreto come età a partire dai 65 anni in su) e applico lo sconto del 40%
         sconto = prezzoBiglietto * 40 / 100;
-        console.log("Sconto del 40%: " + sconto);
+        //console.log("Sconto del 40%: " + sconto);
     }
     //Sottraggo lo sconto al prezzoBiglietto (se lo sconto non è stato applicato sottraggo 0)
     prezzoBiglietto = prezzoBiglietto - sconto;
-    console.log("Il prezzo finale del biglietto è: " + prezzoBiglietto.toFixed(2));
+    //console.log("Il prezzo finale del biglietto è: " + prezzoBiglietto.toFixed(2));
+    document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+    document.getElementById("prezzo_finale").innerHTML = prezzoBiglietto.toFixed(2);
 }
 else{
     //console.log("Errore! Uno dei valori inseriti non era un numero!");
